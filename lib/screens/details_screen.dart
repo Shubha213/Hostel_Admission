89244df9befbd2b1784efbeaaa00451b9a1c9b43
Shubham <shubhamkaspate213@gmail.com>
2022-10-04 02:25:58 +0530

    asd

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -10,13 +10,158 @@ class DetailScreen extends StatefulWidget {
 }
 
 class DetailScreenState extends State<DetailScreen> {
+  String dropdownvalue1 = 'YEAR';
+  String dropdownvalue2 = 'BRANCH';
+  String dropdownvalue3 = 'GENDER';
+
+  // List of items in our dropdown menu
+  var year = [
+    'YEAR',
+    'Item 1',
+    'Item 2',
+    'Item 3',
+    'Item 4',
+    'Item 5',
+  ];
+
+  var branch = [
+    'BRANCH',
+    'Item 1',
+    'Item 2',
+    'Item 3',
+    'Item 4',
+    'Item 5',
+  ];
+
+  var gn = [
+    'GENDER',
+    'MALE',
+    'FEMALE',
+    'Others',
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        backgroundColor: Color(0xFF90294A),
-        title: const Text("Details")),
-    );
+        appBar: AppBar(
+            centerTitle: true,
+            backgroundColor: Color(0xFF90294A),
+            title: const Text("Details")),
+        body: Padding(
+          padding: const EdgeInsets.only(top: 40, left: 40),
+          child: Container(
+            height: 150,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(children: [
+                  Column(
+                    children: const [
+                      Text(
+                        'SELECT YEAR  :-  ',
+                        style: TextStyle(fontSize: 18),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    width: 30,
+                  ),
+                  Column(
+                    children: [
+                      // Text('DropDown',style: TextStyle(fontSize: 18),),
+                      DropdownButton(
+                        value: dropdownvalue1,
+                        icon: const Icon(Icons.keyboard_arrow_down),
+                        items: year.map((String items) {
+                          return DropdownMenuItem(
+                            value: items,
+                            child: Text(items),
+                          );
+                        }).toList(),
+                        onChanged: (String? newValue) {
+                          setState(() {
+                            dropdownvalue1 = newValue!;
+                          });
+                        },
+                      ),
+                    ],
+                  )
+                ]),
+                Row(children: [
+                  Column(
+                    children: const [
+                      Text(
+                        'SELECT BRANCH  :-  ',
+                        style: TextStyle(fontSize: 18),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    width: 30,
+                  ),
+                  Column(
+                    children: [
+                      // Text(
+                      //   'DropDown',
+                      //   style: TextStyle(fontSize: 18),
+                      // ),
+                      DropdownButton(
+                        value: dropdownvalue2,
+                        icon: const Icon(Icons.keyboard_arrow_down),
+                        items: branch.map((String items) {
+                          return DropdownMenuItem(
+                            value: items,
+                            child: Text(items),
+                          );
+                        }).toList(),
+                        onChanged: (String? newValue) {
+                          setState(() {
+                            dropdownvalue2 = newValue!;
+                          });
+                        },
+                      ),
+                    ],
+                  )
+                ]),
+                Row(children: [
+                  Column(
+                    children: const [
+                      Text(
+                        'SELECT YEAR  :-  ',
+                        style: TextStyle(fontSize: 18),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    width: 30,
+                  ),
+                  Column(
+                    children: [
+                      // Text(
+                      //   'DropDown',
+                      //   style: TextStyle(fontSize: 18),
+                      // ),
+                      DropdownButton(
+                        value: dropdownvalue3,
+                        icon: const Icon(Icons.keyboard_arrow_down),
+                        items: gn.map((String items) {
+                          return DropdownMenuItem(
+                            value: items,
+                            child: Text(items),
+                          );
+                        }).toList(),
+                        onChanged: (String? newValue) {
+                          setState(() {
+                            dropdownvalue3 = newValue!;
+                          });
+                        },
+                      ),
+                    ],
+                  )
+                ]),
+              ],
+            ),
+          ),
+        ));
   }
 }
--- lib/screens/user_form.dart
+++ lib/screens/user_form.dart
@@ -2,6 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
+// class UserForm extends StatefulWidget {
+//   const UserForm({Key? key}) : super(key: key);
+
+//   @override
+//   State<UserForm> createState() => _
+// class FormScreen extends StatefulWidget {
+//   @override
+//   State<StatefulWidget> createState() {
+//   return _UserFormState();
+// }
+
 class UserForm extends StatefulWidget {
   const UserForm({Key? key}) : super(key: key);
 
@@ -10,58 +21,258 @@ class UserForm extends StatefulWidget {
 }
 
 class _UserFormState extends State<UserForm> {
-  var _phoneNo;
+  late String _name;
+  late String _email;
+  late String _phoneNumber;
+  late String _course;
+  late String _YOS;
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  List complaintTypeList = [
+    {"title": "IT", "value": "IT"},
+    {"title": "CIVIL", "value": "Civil"},
+    {"title": "ELECT", "value": "Elect"},
+    {"title": "MECH", "value": "Mech"},
+    {"title": "ENTC", "value": "Entc"},
+  ];
+
+  List YearofStudy = [
+    {"title": "I", "value": "1"},
+    {"title": "II", "value": "2"},
+    {"title": "III", "value": "3"},
+    {"title": "IV", "value": "4"},
+    // {"title": "ENTC", "value": "Entc"},
+  ];
+
+  String defaultValue = "";
+
+  Widget _buildName() {
+    return TextFormField(
+      decoration: InputDecoration(
+        labelText: 'Name : ',
+        labelStyle: TextStyle(color: Colors.black),
+        enabledBorder: OutlineInputBorder(
+          borderSide:
+              BorderSide(color: Color.fromARGB(173, 22, 22, 22), width: 2),
+          borderRadius: BorderRadius.circular(16),
+        ),
+        fillColor: Color.fromARGB(143, 253, 251, 251),
+        filled: true,
+        focusedBorder: OutlineInputBorder(
+          borderSide:
+              BorderSide(color: Color.fromARGB(173, 22, 22, 22), width: 2),
+          borderRadius: BorderRadius.circular(16),
+        ),
+      ),
+      validator: (String? value) {
+        if (value!.isEmpty) {
+          return 'Name is Required';
+        }
+
+        return null;
+      },
+      onSaved: (value) {
+        _name = value!;
+      },
+    );
+  }
+
+  Widget _buildEmail() {
+    return TextFormField(
+      decoration: InputDecoration(
+        labelText: 'Email : ',
+        labelStyle: TextStyle(color: Colors.black),
+        enabledBorder: OutlineInputBorder(
+          borderSide:
+              BorderSide(color: Color.fromARGB(173, 22, 22, 22), width: 2),
+          borderRadius: BorderRadius.circular(16),
+        ),
+        fillColor: Color.fromARGB(143, 253, 251, 251),
+        filled: true,
+        focusedBorder: OutlineInputBorder(
+          borderSide:
+              BorderSide(color: Color.fromARGB(173, 22, 22, 22), width: 2),
+          borderRadius: BorderRadius.circular(16),
+        ),
+      ),
+      validator: (value) {
+        if (value!.isEmpty) {
+          return 'Email is Required';
+        }
+
+        if (!RegExp(
+                r"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")
+            .hasMatch(value)) {
+          return 'Please enter a valid email Address';
+        }
+
+        return null;
+      },
+      onSaved: (value) {
+        _email = value!;
+      },
+    );
+  }
+
+  Widget _buildPhoneNumber() {
+    return TextFormField(
+      decoration: InputDecoration(
+        labelText: 'Phone Number : ',
+        labelStyle: TextStyle(color: Colors.black),
+        enabledBorder: OutlineInputBorder(
+          borderSide:
+              BorderSide(color: Color.fromARGB(173, 22, 22, 22), width: 2),
+          borderRadius: BorderRadius.circular(16),
+        ),
+        fillColor: Color.fromARGB(143, 253, 251, 251),
+        filled: true,
+        focusedBorder: OutlineInputBorder(
+          borderSide:
+              BorderSide(color: Color.fromARGB(173, 22, 22, 22), width: 2),
+          borderRadius: BorderRadius.circular(16),
+        ),
+      ),
+      keyboardType: TextInputType.phone,
+      validator: (value) {
+        if (value!.isEmpty) {
+          return 'Phone number is Required';
+        }
+
+        return null;
+      },
+      onSaved: (value) {
+        _phoneNumber = value!;
+      },
+    );
+  }
+
+  Widget _courseSelect() {
+    return Container(
+      width: 130,
+      child: DropdownButtonFormField(
+          decoration: InputDecoration(
+            labelStyle: TextStyle(color: Colors.black),
+            enabledBorder: OutlineInputBorder(
+              borderSide:
+                  BorderSide(color: Color.fromARGB(173, 22, 22, 22), width: 2),
+              borderRadius: BorderRadius.circular(16),
+            ),
+            fillColor: Color.fromARGB(143, 253, 251, 251),
+            filled: true,
+            focusedBorder: OutlineInputBorder(
+              borderSide:
+                  BorderSide(color: Color.fromARGB(173, 22, 22, 22), width: 2),
+              borderRadius: BorderRadius.circular(16),
+            ),
+          ),
+          value: defaultValue,
+          //isExpanded: true,
+
+          items: [
+            const DropdownMenuItem(child: Text("Course"), value: ""),
+            ...complaintTypeList.map<DropdownMenuItem<String>>((e) {
+              return DropdownMenuItem(
+                  child: Text(e['title']), value: e['value']);
+            }).toList(),
+          ],
+          onChanged: (value) {
+            setState(() => _course = value.toString());
+          }),
+    );
+  }
+
+  Widget _yoStudy() {
+    return Container(
+      width: 130,
+      child: DropdownButtonFormField(
+          decoration: InputDecoration(
+            labelStyle: TextStyle(color: Colors.black),
+            enabledBorder: OutlineInputBorder(
+              borderSide:
+                  BorderSide(color: Color.fromARGB(173, 22, 22, 22), width: 2),
+              borderRadius: BorderRadius.circular(16),
+            ),
+            fillColor: Color.fromARGB(143, 253, 251, 251),
+            filled: true,
+            focusedBorder: OutlineInputBorder(
+              borderSide:
+                  BorderSide(color: Color.fromARGB(173, 22, 22, 22), width: 2),
+              borderRadius: BorderRadius.circular(16),
+            ),
+          ),
+          value: defaultValue,
+          //isExpanded: true,
+
+          items: [
+            const DropdownMenuItem(child: Text("Course"), value: ""),
+            ...YearofStudy.map<DropdownMenuItem<String>>((e) {
+              return DropdownMenuItem(
+                  child: Text(e['title']), value: e['value']);
+            }).toList(),
+          ],
+          onChanged: (value) {
+            setState(() => _YOS = value.toString());
+          }),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          backgroundColor: Color(0xFF90294A),
-          title: const Text(
-            "Application Form",
-            style: TextStyle(fontSize: 24, fontFamily: 'YOUR_FONT_FAMILY'),
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Color(0xFF90294A),
+        title: const Text(
+          "Application Form",
+          style: TextStyle(fontSize: 24, fontFamily: 'YOUR_FONT_FAMILY'),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          margin: EdgeInsets.all(18),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                _buildName(),
+                SizedBox(height: 20),
+                _buildEmail(),
+                SizedBox(height: 20),
+                _buildPhoneNumber(),
+                SizedBox(height: 20),
+                Row(
+                  children: [
+                    _courseSelect(),
+                  ],
+                ),
+                SizedBox(height: 100),
+                RaisedButton(
+                  child: Text(
+                    'Submit',
+                    style: TextStyle(color: Colors.blue, fontSize: 16),
+                  ),
+                  onPressed: () {
+                    if (!_formKey.currentState!.validate()) {
+                      return;
+                    }
+
+                    _formKey.currentState!.save();
+
+                    print(_name);
+                    print(_email);
+                    print(_phoneNumber);
+                    print(_course);
+
+                    //Send to API
+                  },
+                )
+              ],
+            ),
           ),
         ),
-        body: Padding(
-          padding: const EdgeInsets.only(top: 20, left: 10, right: 10),
-          child: Column(
-            children: [
-              Form(
-                child: Container(
-                  child: Column(
-                    children: [
-                      SizedBox(
-                        height: 46,
-                        child: TextFormField(
-                          controller: _phoneNo,
-                          decoration: InputDecoration(
-                            labelText: 'Phone Number: ',
-                            labelStyle: TextStyle(color: Colors.black),
-                            enabledBorder: OutlineInputBorder(
-                              borderSide: BorderSide(
-                                  color: Color.fromARGB(173, 22, 22, 22),
-                                  width: 2),
-                              borderRadius: BorderRadius.circular(17),
-                            ),
-                            fillColor: Color.fromARGB(143, 253, 251, 251),
-                            filled: true,
-                            focusedBorder: OutlineInputBorder(
-                              borderSide: BorderSide(
-                                  color: Color.fromARGB(173, 22, 22, 22),
-                                  width: 2),
-                              borderRadius: BorderRadius.circular(18),
-                            ),
-                          ),
-                          keyboardType: TextInputType.phone,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ));
+      ),
+    );
   }
 }
